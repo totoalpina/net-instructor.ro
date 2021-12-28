f3d30eb56fa533690c8ec8d085b420575e0ccf51
totoalpina <totoalpina@gmail.com>
2021-12-28 09:22:51 +0200

    Modificat cateva fisiere

--- application/routes.py
+++ application/routes.py
@@ -1,14 +1,15 @@
 import os
-from flask import render_template, redirect, url_for, flash, request
-from application import app, db, login_manager, mail
-from application.models import LoginForm, SignupForm, Login, Clienti, CursantNouForm, ContactMe, UpdateUser
 from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
+
+from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_required, current_user, login_user, logout_user
 from flask_mail import Message
 from itsdangerous import URLSafeTimedSerializer, SignatureExpired
-from application.functii import validare_mail
+from werkzeug.security import generate_password_hash, check_password_hash
 
+from application import app, db, login_manager, mail
+from application.functii import validare_mail
+from application.models import LoginForm, SignupForm, Login, Clienti, CursantNouForm, ContactMe, UpdateUser
 
 s = URLSafeTimedSerializer('SECRET_KEY')
 
@@ -17,22 +18,23 @@ s = URLSafeTimedSerializer('SECRET_KEY')
 def load_user(login_id):
     return Login.query.get(int(login_id))
 
-@app.route('/', methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     an_curent = datetime.now()
     return render_template('index.html', an_curent=an_curent)
 
 
-@app.route('/logare', methods=['GET','POST'])
+@app.route('/logare', methods=['GET', 'POST'])
 def logare():
     form = LoginForm()
     an_curent = datetime.now()
-    if form.validate_on_submit() :
+    if form.validate_on_submit():
         utilizator = Login.query.filter_by(utilizator=form.utilizator.data).first()
-        print( [utilizator])
 
         if utilizator:
             if check_password_hash(utilizator.parola, form.parola.data) and utilizator.activ == 1:
+
                 login_user(utilizator)
                 flash("V-ati logat cu succes")
 
@@ -40,35 +42,38 @@ def logare():
 
             elif not check_password_hash(utilizator.parola, form.parola.data):
 
-                flash ( "Parola introdusa nu este corecta. Incercati din nou" )
+                flash("Parola introdusa nu este corecta. Incercati din nou")
                 return redirect(url_for('logare'))
 
-            else :
-
+            else:
                 flash("Contul dumneavoastra nu este activ !!! Contactati-ne pentru a verifica situatia contului.")
             return redirect(url_for('index'))
 
-        flash("Nu aveti dreptul de a folosi aceasta sectiune. Creati-va in cont pentru a pute intra !")
+        flash("Nu aveti dreptul de a folosi aceasta sectiune. Creati-va in cont pentru a putea intra !")
     return render_template("logare.html", form=form, an_curent=an_curent)
 
 
-
-@app.route('/cont_nou', methods=['GET','POST'])
+@app.route('/cont_nou', methods=['GET', 'POST'])
 def cont_nou():
     form = SignupForm()
     an_curent = datetime.now()
     if form.validate_on_submit():
-        email=form.email.data
-        hased_parola = generate_password_hash(form.parola.data, method='sha256' )
-        utilizator_nou = Login(nume=form.nume.data, prenume=form.prenume.data, utilizator=form.utilizator.data, email=form.email.data,
-                              data_adaugare=datetime.now(), data_modificare=datetime.now(), parola=hased_parola)
+        email = form.email.data
+        hashed_parola = generate_password_hash(form.parola.data, method='sha256')
+        utilizator_nou = Login(nume=form.nume.data,
+                               prenume=form.prenume.data,
+                               utilizator=form.utilizator.data,
+                               email=form.email.data,
+                               data_adaugare=datetime.now(),
+                               data_modificare=datetime.now(),
+                               parola=hashed_parola)
 
         validare_mail(email)
 
         if form.parola.data == form.confirma_parola.data:
             db.session.add(utilizator_nou)
             db.session.commit()
-            email=utilizator_nou.email
+            email = utilizator_nou.email
             token = s.dumps(email, salt='confirmare_email')
 
             msg = Message('Confirmare Email', sender='totoalpina@gmail.com', recipients=[email])
@@ -85,6 +90,7 @@ def cont_nou():
         return redirect(url_for('cont_nou'))
     return render_template('cont_nou.html', form=form, an_curent=an_curent)
 
+
 @app.route('/confirmare_email/<token>')
 def confirmare_email(token):
     try:
@@ -100,97 +106,110 @@ def confirmare_email(token):
         return '<h1>The token is expired!</h1>'
     return redirect(url_for('logare'))
 
-@app.route('/profil_utilizator', methods=['GET','POST'])
+
+@app.route('/profil_utilizator', methods=['GET', 'POST'])
 @login_required
 def profil_utilizator():
-
-    form = UpdateUser ( )
-    if form.validate_on_submit ( ) :
+    form = UpdateUser()
+    if form.validate_on_submit():
         id = current_user.id
         utilizator = form.utilizator.data
         nume = form.nume.data
         prenume = form.prenume.data
         email = form.email.data
         val_noi = Login.query.filter(Login.id == id).first()
-        val_noi.utilizator=utilizator
-        val_noi.nume=nume
-        val_noi.prenume=prenume
-        val_noi.email=email
-        val_noi.data_modificare=datetime.now()
+        val_noi.utilizator = utilizator
+        val_noi.nume = nume
+        val_noi.prenume = prenume
+        val_noi.email = email
+        val_noi.data_modificare = datetime.now()
         db.session.flush()
-        db.session.commit ( )
+        db.session.commit()
         flash('Profilul a fost modificat cu noile valori')
-    elif request.method == 'GET' :
+    elif request.method == 'GET':
         form.utilizator.data = current_user.utilizator
         form.nume.data = current_user.nume
         form.prenume.data = current_user.prenume
         form.email.data = current_user.email
         id = current_user.id
 
-    return render_template("profil_utilizator.html", form=form, an_curent=datetime.now ( ))
+    return render_template("profil_utilizator.html", form=form, an_curent=datetime.now())
 
 
-@app.route('/clienti', methods=['GET','POST'])
+@app.route('/clienti', methods=['GET', 'POST'])
 @login_required
 def clienti():
     an_curent = datetime.now()
     form = Clienti.query.order_by('id')
-    return render_template("clienti.html", an_curent=an_curent, form = form)
+    return render_template("clienti.html", an_curent=an_curent, form=form)
 
-@app.route('/detalii_cursant/<int:id>', methods=['GET','POST'])
+
+@app.route('/detalii_cursant/<int:id>', methods=['GET', 'POST'])
 @login_required
 def detalii_cursant(id):
     cursant = Clienti.query.filter_by(id=id).first()
     print(cursant)
     return render_template('detalii_cursant.html', cursant=cursant, an_curent=datetime.now())
 
-@app.route('/adauga_cursant', methods=['GET','POST'])
+
+@app.route('/adauga_cursant', methods=['GET', 'POST'])
 @login_required
 def adauga_cursant():
-    an_curent=datetime.now()
+    an_curent = datetime.now()
     form = CursantNouForm()
     return render_template("adauga_cursant.html", form=form, an_curent=an_curent)
 
-@app.route('/cursant', methods=['GET','POST'])
+
+@app.route('/cursant', methods=['GET', 'POST'])
 @login_required
 def cursant():
     form = CursantNouForm()
     an_curent = datetime.now()
     if form.validate_on_submit:
-        cursant_nou = Clienti(nume=form.nume.data, prenume=form.prenume.data, categorie_scolarizare=form.categorie_scolarizare.data,
-                                     localitate=form.localitate.data, adresa=form.adresa.data, email=form.email.data,
-                                     telefon=form.telefon.data, data_adaugare=datetime.now(), data_modificare=datetime.now(),
-                                     observatii=form.observatii.data, activ=1)
+        cursant_nou = Clienti(nume=form.nume.data,
+                              prenume=form.prenume.data,
+                              categorie_scolarizare=form.categorie_scolarizare.data,
+                              localitate=form.localitate.data,
+                              adresa=form.adresa.data,
+                              email=form.email.data,
+                              telefon=form.telefon.data,
+                              data_adaugare=datetime.now(),
+                              data_modificare=datetime.now(),
+                              observatii=form.observatii.data,
+                              activ=1)
         db.session.add(cursant_nou)
         db.session.commit()
         return redirect(url_for('clienti'))
     return render_template("adauga_cursant.html", form=form, an_curent=an_curent)
 
-@app.route('/contact_me', methods=['GET','POST'])
+
+@app.route('/contact_me', methods=['GET', 'POST'])
 def contact_me():
     form = ContactMe()
-    return render_template("contact.html", form=form, an_curent = datetime.now ( ))
+    return render_template("contact.html", form=form, an_curent=datetime.now())
 
-@app.route('/send_mail', methods=['GET','POST'])
+
+@app.route('/send_mail', methods=['GET', 'POST'])
 def send_mail():
-    form = ContactMe ( )
+    form = ContactMe()
     email = form.email.data
 
     validare_mail(email)
 
     send_to = os.environ.get('MAIL_USERNAME')
-    if form.validate_on_submit ( ) :
-        try :
-            msg = Message ( 'Cineva te vrea !! Formular contact Net-Instructor.ro', sender = (email), recipients = [send_to] )
+    if form.validate_on_submit():
+        try:
+            msg = Message('Cineva te vrea !! Formular contact Net-Instructor.ro', sender=(email), recipients=[send_to])
             msg.body = '\n from : ' + form.nume.data + ' - telefon: ' + form.telefon.data + ', \n \n ' + form.mesaj.data
 
-            mail.send ( msg )
-            flash (
-                "Mesajul dvs a for trimis. Va multumim pentru increderea acordata. Veti fi contactat in cel mai scurt timp posibil." )
+            mail.send(msg)
+            flash(
+                "Mesajul dvs a for trimis. Va multumim pentru increderea acordata. Veti fi contactat in cel mai scurt timp posibil.")
             return redirect(url_for('contact_me'))
         except:
-            flash ( "Completati toate campurile si incarcati din nou!")
-    return redirect ( url_for ( 'contact_me' ) )
+            flash("Completati toate campurile si incarcati din nou!")
+    return redirect(url_for('contact_me'))
+
 
 @app.route('/sterge_cursant/<int:id>')
 @login_required
@@ -200,6 +219,7 @@ def sterge_cursant(id):
     db.session.commit()
     flash("Cursant sters cu succes")
     return redirect(url_for('clienti'))
+
 
 @app.route('/delogare')
 @login_required
--- config.py
+++ config.py
@@ -3,14 +3,21 @@ import os
 from datetime import timedelta
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS = True
+    SECRET_KEY = "maricicabiciclista"
+    MAIL_SERVER = "mail.zenic.ro"
+    MAIL_USERNAME = "cosmin@zenic.ro"
+    MAIL_PASSWORD = "0bR_oaXi;fe!"
+    MAIL_PORT = "587"
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    # SECRET_KEY = os.environ.get('SECRET_KEY')
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    # MAIL_PORT = os.environ.get('MAIL_PORT')
+    # MAIL_USE_TLS = True
+
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:armand27octombrie@localhost:3306/net_instructor"
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     PERMANENT_SESSION_LIFETIME = timedelta ( minutes = 20 )
     SESSION_REFRESH_EACH_REQUEST = True
     # SQLALCHEMY_BINDS = {
